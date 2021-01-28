57086c35cf2b73dc01501ac2ef1ab63ef218338f
hfg-luca-a <72493170+hfg-luca-a@users.noreply.github.com>
2021-01-28 14:30:38 +0100

    git repo umzug

---
+++ script.js
@@ -0,0 +1,412 @@
+//declarieren von globalen variablen
+let currentplayer;
+let p1 = "X";
+let p2 = "O";
+let gamearry = [];
+let gamesize = 0;
+let p1points = 0;
+let p2points = 0;
+let pointstowin = 0;
+let switchvar = true;
+let posarry = [];
+//--------------------------------------
+
+
+// wird in der html datei aufgerufen und im programmcode unter resetfield()
+/* jshint ignore:start */
+
+function generate() {
+
+    // lässt input und button verschwinden 
+    document.getElementById("pointstowin").setAttribute('hidden', true);
+    document.getElementById("size").setAttribute('hidden', true);
+    document.getElementById("generatebtn").setAttribute('hidden', true);
+    //leert das gamearray
+    gamearry.splice(0, gamearry.length);
+
+    let counter = 0;
+    let newId = 0;
+
+    $("tr").remove(); // löscht alle td elemente
+    // holt sich die größe des Spielfeldes
+    gamesize = Number(document.getElementById("size").value);
+    pointstowin = Number(document.getElementById("pointstowin").value);
+    //generiert spalten
+    for (let i = 0; i < gamesize; i++) {
+        newId = "Zeile" + i
+        $("table").append("<tr id=" + newId + "></tr>");
+        gamearry[i] = new Array(gamesize);
+        //füllt spalten
+        for (let j = 0; j < gamesize; j++) {
+            let idvar = String(i) + "-" + String(j)
+            $("#" + newId).append("<td id=" + idvar + "></td>");
+            counter++;
+
+        }
+    }
+}
+/* jshint ignore:end */
+
+$("table").on("click", "td", function () { //wird ausgeführt wenn of ein td element geklickt wird
+
+
+    if ($(this).text() === "") { // wenn in dem td element noch kein wert drin ist kann er beschrieben werden 
+
+        $(this).text(changeplayer()); // schreibt den Spielr in das Element der aktuell an der Reihe ist
+        let id = $(this).attr("id"); // holt sich die id des geklickten td Elements
+        posarry = id.split("-"); // schreibt die Positions werte in ein temp. array
+        gamearry[posarry[0]][posarry[1]] = currentplayer; // schreibt  an die stelle des gamearrys den aktuellen Spieler 
+        // console.log(gamearry)
+        checkunentschieden();
+        checkwin(posarry);
+        checkdiagonal();
+
+    }
+
+
+    if (document.getElementById("player2").value === "Bot") {
+        setTimeout(function () {
+            placingBot();
+        }, 500);
+
+    }
+
+    // console.log($(this).attr("id"));
+});
+
+
+
+function changeplayer() { // gibt X oder O bei aufruf zurück, je nach vorherigem status 
+
+    if (currentplayer === "X") {
+
+        currentplayer = "O";
+        // Dreht den Pfeil zum dem der an der Reihe ist
+        document.getElementById("arrow").innerHTML = "&#8592;"
+    } else {
+
+        currentplayer = "X";
+        // Dreht den Pfeil zum dem der an der Reihe ist
+        document.getElementById("arrow").innerHTML = "&#8594;"
+    }
+    return currentplayer
+}
+// wird in der html datei aufgerufen
+/* jshint ignore:start */
+function setPlayer() {
+    //Spieler inputs können nun nicht mehr beschrieben werden 
+    const p1 = document.getElementById("player1");
+    const p2 = document.getElementById("player2");
+    p1.setAttribute('disabled', true);
+    p2.setAttribute('disabled', true);
+    // Button Setplayer verschwindet
+    document.getElementById("setplayerbtn").setAttribute('hidden', true);
+}
+/* jshint ignore:end */
+
+
+
+
+function checkwin(button) {
+
+    // console.log(button)
+    const posx = Number(button[0]);
+    const posy = Number(button[1]);
+    //console.log(posx, posy, typeof posx, typeof posy)
+    let winarryh = [],
+        winarryv = [],
+        hpos = [],
+        vpos = []; // Statements reduzieren
+    for (let index = 0; index < gamesize; index++) {
+
+        if (gamearry[posx][posy] === gamearry[posx][index]) {
+            hpos.push({
+                posx: posx,
+                posy: index
+            });
+            winarryh.push(gamearry[posx][posy]);
+            if (winarryh.length >= pointstowin) {
+                winner(currentplayer);
+                showcase(hpos);
+                break;
+            }
+        } else {
+            winarryh.splice(0, winarryh.length);
+        }
+    }
+
+    for (let index = 0; index < gamesize; index++) {
+
+        if (gamearry[posx][posy] === gamearry[index][posy]) {
+            vpos.push({
+                posx: index,
+                posy: posy
+            })
+            winarryv.push(gamearry[posx][posy])
+            if (winarryv.length >= pointstowin) {
+                winner(currentplayer)
+                showcase(vpos)
+                break;
+            }
+        } else {
+            winarryv.splice(0, winarryv.length)
+        }
+    }
+}
+
+function generatewincon(zeichen) {
+    let tempvar = "";
+    for (let index = 0; index < pointstowin; index++) {
+        tempvar += zeichen;
+    }
+    return tempvar;
+}
+
+function checkdiagonal() {
+
+    let winconditiond1 = generatewincon(p1),
+        winconditiond2 = generatewincon(p2),
+        curLineD = "",
+        curLineD2 = "",
+        d1pos = [],
+        d2pos = [];
+
+    for (let i = 0; i < gamesize; i++) {
+
+        for (let j = 0; j < gamesize; j++) {
+
+            curLineD = "";
+            curLineD2 = "";
+            d1pos.splice(0, d1pos.length);
+            d2pos.splice(0, d2pos.length);
+
+            if ((i + pointstowin - 1) < gamesize) {
+                if ((j + pointstowin - 1) < gamesize) {
+                    for (let m = 0; m < pointstowin; m++) {
+                        curLineD += gamearry[i + m][j + m]
+                        d1pos.push({
+                            posx: i + m,
+                            posy: j + m
+                        })
+                    }
+                    if (curLineD.includes(winconditiond1)) {
+                        showcase(d1pos)
+                        winner(p1);
+                        sperren()
+                    } else if (curLineD.includes(winconditiond2)) {
+                        showcase(d1pos)
+                        winner(p2)
+                        sperren()
+                    }
+                }
+            }
+            if (j - (pointstowin - 1) >= 0) {
+                if (i + (pointstowin - 1) < gamesize) {
+                    for (let m = 0; m < pointstowin; m++) {
+                        curLineD2 += gamearry[i + m][j - m]
+                        d2pos.push({
+                            posx: i + m,
+                            posy: j - m
+                        })
+                    }
+                    if (curLineD2.includes(winconditiond1)) {
+                        showcase(d2pos)
+                        winner(p1);
+
+                    } else if (curLineD2.includes(winconditiond2)) {
+                        showcase(d2pos)
+                        winner(p2)
+                    }
+                }
+            }
+        }
+    }
+}
+
+function sperren() {
+    switchvar = false;
+    $("td").prop("disabled", true);
+
+}
+
+function showcase(posofwincells) {
+    console.log(posofwincells)
+    for (const item of posofwincells) {
+        let x = item.posx
+        let y = item.posy
+        let pos = `${x}-${y}`;
+        document.getElementById(pos).style.background = "lightgreen";
+    }
+}
+
+function checkunentschieden() {
+    let counter = 0
+    console.log(gamearry)
+    for (let x = 0; x < gamesize; x++) {
+        for (let y = 0; y < gamesize; y++) {
+            if (gamearry[x][y] === undefined) {
+                counter++;
+            }
+
+
+        }
+
+
+    }
+    if (counter === 0) {
+        alert('Unentschieden')
+        winner("none")
+    }
+
+}
+
+function winner(zeichen) {
+    sperren()
+    //schaut wer gewonnen hat
+    if (zeichen === "X") {
+        //schreibt die aktuelle punktzahl in das span tag
+        console.log(document.getElementById("player1").value + " hat gewonnen")
+        p1points++;
+        document.getElementById("punktep1").innerText = p1points;
+
+
+    } else if (zeichen === "none") {
+        //placeholder
+    } else {
+        p2points++;
+        console.log(document.getElementById("player2").value + " hat gewonnen")
+        document.getElementById("punktep2").innerText = p2points;
+
+    }
+    //lässt den neue Runde Button erscheinen
+    document.getElementById("neuerundebtn").removeAttribute('hidden');
+    // dreht den Pfeil nach unten
+    document.getElementById("arrow").innerHTML = "&#8595;"
+    //console.log("Das Zeichen: "+ zeichen + " hat gewonnen")
+}
+
+//wird in der html datei aufgerufen -> heißt jshint trifft nicht zu
+/* jshint ignore:start */
+function resetfield() {
+    switchvar = true;
+    //richtet den Pfeil aus
+    if (currentplayer !== "X") {
+        document.getElementById("arrow").innerHTML = "&#8592;"
+    } else {
+        document.getElementById("arrow").innerHTML = "&#8594;"
+    }
+    // lässt Button für neue runde verschwinden 
+    document.getElementById("neuerundebtn").setAttribute('hidden', true);
+    //generiert das Spielfeld neu
+    generate();
+}
+/* jshint ignore:end */
+
+function getRandomInt(max) { //Funktion für eine Zufallszahl
+    return Math.floor(Math.random() * Math.floor(max));
+}
+
+function placingBot() { //Zufalls Bot
+    //generiert eine Zufällige x y Koordinate
+    if (switchvar === true) {
+        let y = getRandomInt(gamesize)
+        let x = getRandomInt(gamesize)
+        if (gamearry[x][y] === undefined) {
+            const temparry = [x, y]
+            console.error(y + "|" + x)
+            //beschreibt ein td tag mit O
+            let randomtdtag = document.getElementById(`${x}-${y}`)
+            $(randomtdtag).prop("disabled", true);
+            const temp = changeplayer()
+            randomtdtag.innerText = temp
+            //setzt an der gleichen stelle im gamearry ein O
+            gamearry[x][y] = temp
+            console.warn(x + ":" + y + "|" + document.getElementById(`${x}-${y}`).innerHTML)
+            checkunentschieden()
+            checkwin(temparry)
+            return
+        } else {
+            placingBot()
+            console.error("Generating new place to be")
+        }
+    }
+}
+
+
+
+/**
+ * 
+ * Alte Gewinnabfrage
+ * 
+ */
+//the old shit
+/**
+ * function determineWin(pressedbtn) {
+    console.log(gamesize)
+    console.log($(pressedbtn).text())
+    const gecklicktepos = pressedbtn.split(",")
+    console.log(gecklicktepos);
+
+
+
+    let checkarry = [];
+    let vcheckarry = [];
+    let dcheckarry = [];
+    let d2checkarry = [];
+
+    for (let x = 0; x < gamesize; x++) {
+
+        if (gamearry[gecklicktepos[0]][gecklicktepos[1]] === gamearry[gecklicktepos[0]][x]) {
+            checkarry.push(gamearry[gecklicktepos[0]][x])
+            if (checkarry.length === gamesize) {
+                winner(gamearry[gecklicktepos[0]][x])
+            }
+
+        }
+
+    }
+    //  console.log(checkarry)
+
+    for (let y = 0; y < gamesize; y++) {
+
+        if (gamearry[gecklicktepos[0]][gecklicktepos[1]] === gamearry[y][gecklicktepos[1]]) {
+            vcheckarry.push(gamearry[y][gecklicktepos[1]])
+            if (vcheckarry.length === gamesize) {
+                winner(gamearry[y][gecklicktepos[1]])
+            }
+
+        }
+
+    }
+    //console.log(vcheckarry)
+
+    for (let y = 0; y < gamesize; y++) {
+
+        if (gamearry[gecklicktepos[0]][gecklicktepos[1]] === gamearry[y][y]) {
+            dcheckarry.push(gamearry[y][y])
+            if (dcheckarry.length === gamesize) {
+                winner(gamearry[y][y])
+            }
+
+        }
+
+    }
+    //  console.log(dcheckarry)
+    let testb = 0;
+    let test2 = gamesize - 1
+    for (let y = 0; y < gamesize; y++) {
+        //console.log(test)
+        // console.log(test2)
+        if (gamearry[gecklicktepos[0]][gecklicktepos[1]] === gamearry[test2][testb]) {
+            d2checkarry.push(gamearry[test2][testb])
+            if (d2checkarry.length === gamesize) {
+                winner(gamearry[test2][testb])
+            }
+        }
+        test++;
+        test2--;
+    }
+    // console.log(d2checkarry)
+}
+ * 
+ */
